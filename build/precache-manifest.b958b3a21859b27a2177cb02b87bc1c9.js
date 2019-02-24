fb06936c8356b36b6fb0839039f707bb6ecaf413
Ainnop <ainnop@Ainnops-MacBook-Pro.local>
2019-02-24 14:07:38 +0300

    Atwau Surge Commit

---
+++ build/precache-manifest.b958b3a21859b27a2177cb02b87bc1c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "2ada5de585db64a2e3c2",
+    "url": "/static/js/main.2ada5de5.chunk.js"
+  },
+  {
+    "revision": "063ce5bc9f8a30d66648",
+    "url": "/static/js/2.063ce5bc.chunk.js"
+  },
+  {
+    "revision": "2ada5de585db64a2e3c2",
+    "url": "/static/css/main.0151777d.chunk.css"
+  },
+  {
+    "revision": "579779f18c8862a1d5cc96605d51f1db",
+    "url": "/index.html"
+  }
+];
