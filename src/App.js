0e6d911ceede3b5f47009c52e88304f442c13ad4
Ainnop <ainnop@Ainnops-MacBook-Pro.local>
2019-02-24 13:30:23 +0300

    Atwau React App first github commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+
+  state = {
+    persons: [
+      {name: 'James', age: 29},
+      {name: 'Atwau', age: 21},
+      {name: 'Arereng', age: 12}
+    ],
+    otherState: 'Some other value'
+  }
+
+  switchNameHandler = () =>{
+    //console.log('Was Clicked');
+    this.setState({persons: [
+      {name: 'Polo Atwau', age: 29},
+      {name: 'Atwau', age: 21},
+      {name: 'Arereng', age: 12}
+    ]})
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Atwau is calling</h1>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <p className="App-intro">
+          My name is Arereng Jemimah. Welcome, and hello!
+        </p>
+      </div>
+    );
+
+    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'This is atwau calling'));
+  }
+}
+
+export default App;
